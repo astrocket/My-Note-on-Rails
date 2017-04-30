5f21d36e025565ea69920258280b72b0208135bf
Astrocket <leonardo.lee1993@gmail.com>
2017-04-30 20:15:13 +0000

    validate error fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ActiveRecord::Base
-    validates :title, :writer, :content, presence: true, :message => "제목, 글쓴이, 내용 중 하나가 비어 있습니다."
+    validates :title, :writer, :content, presence: { :message => "제목, 글쓴이, 내용 중 하나가 비어 있습니다." }
 end
